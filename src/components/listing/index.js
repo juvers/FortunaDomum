2440397981fbe598b4d815c70d9142fc29b56ad0
Jude kuti <judekuti@gmail.com>
2020-08-15 04:46:40 -0500

    Improve features

--- src/components/listing/index.js
+++ src/components/listing/index.js
@@ -2,24 +2,30 @@ import React from 'react'
 import { Link } from '@reach/router'
 import classnames from 'classnames'
 
+import { asCurrency } from '../../utils/number'
+import styles from './styles.module.css'
+
 function Listing({ listing }) {
   if (!listing) {
     return null
   }
 
   const { id, image, title, address, description, price } = listing
-  const columnClasses = classnames('column', 'col-4', 'col-xs-12')
-  const cardClasses = classnames('card')
+  const columnClasses = classnames('column', 'col-4', 'col-xs-12', styles.column)
+  const cardClasses = classnames('card', styles.card)
 
   return (
-    <div className={columnClasses} style={{ margin: '1rem 0' }}>
+    <div className={columnClasses}>
       <div className={cardClasses}>
         <div className="card-image">
           <img className="img-responsive" src={`/server/${image}`} alt={address} />
         </div>
         <div className="card-header">
           <div className="card-title h5">{title}</div>
-          <div className="card-title h6">${price}</div>
+          <div className="card-title h6">
+            &pound;
+            {asCurrency(price)}
+          </div>
           <div className="card-subtitle text-gray">{address}</div>
         </div>
         <div className="card-body">{description}</div>
