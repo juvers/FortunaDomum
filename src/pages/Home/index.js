5c6296761d956e3b95fd6e50bb5ae24b1786173c
Jude kuti <judekuti@gmail.com>
2020-08-15 04:37:45 -0500

    Import base component into home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import {
-    PropertyListingsProvider,
-    PropertyListingsConsumer
-  } from '../../context/PropertyListingsProvider';
-import Listing from '../../components/listing';
-import Filter from '../../components/filter';
-import Hero from '../../components/hero';
+import React from 'react'
 
+import {
+  PropertyListingsProvider,
+  PropertyListingsConsumer
+} from '../../context/PropertyListingsProvider'
+
+import BaseLayout from '../../components/baseLayout'
+import Listing from '../../components/listing'
+import Filter from '../../components/filter'
 
 function Home() {
-  return(
-    <>
-    <Hero />
-    <div className="container">
-    <Filter />
+  return (
+    <BaseLayout>
+      <div className="container">
         <PropertyListingsProvider>
-            <PropertyListingsConsumer>
-                {({ propertyListings }) => (
-                    <ul>
-                    {propertyListings.map(listing => (
-                        <Listing key={listing.id} listing={listing} />
-                    ))}
-                    </ul>
-                )}
-            </PropertyListingsConsumer>
+          <PropertyListingsConsumer>
+            {({ propertyListings, allListings, updateFilter }) => (
+              <>
+                <Filter
+                  updateFilter={updateFilter}
+                  count={propertyListings.length}
+                  postcodes={allListings
+                    .map(listing => listing.postcode.split(' ')[0])
+                    .filter((item, i, arr) => arr.indexOf(item) === i)}
+                />
+                <div className="columns">
+                  {propertyListings.map(listing => (
+                    <Listing listing={listing} />
+                  ))}
+                </div>
+              </>
+            )}
+          </PropertyListingsConsumer>
         </PropertyListingsProvider>
-    </div>
-    </>
-  ) 
-  
+      </div>
+    </BaseLayout>
+  )
 }
 
 export default Home
