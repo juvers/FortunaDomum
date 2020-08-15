ce992378b1a8aa9bab6a1204a7c309dc0d45a61e
Jude kuti <judekuti@gmail.com>
2020-08-15 03:02:16 -0500

    Create context, afford Provider the value to be shared and make Consumer receive values

---
+++ src/context/PropertyListingsProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, Component } from 'react'
+
+const DefaultState = {
+    propertyListings: []
+}
+
+const PropertyListingsContext = createContext(DefaultState)
+
+export const PropertyListingsConsumer = PropertyListingsContext.Consumer;
+
+export class PropertyListingsProvider extends React.Component {
+    state = DefaultState
+  
+    componentDidMount() {
+      fetch('http://localhost:3000/data')
+        .then(res => res.json())
+        .then(res => {
+          this.setState({ propertyListings: res })
+        })
+    }
+  
+    render() {
+      const { children } = this.props
+      const { propertyListings } = this.state
+  
+      return (
+        <PropertyListingsContext.Provider
+          value={{
+            propertyListings
+          }}
+        >
+          {children}
+        </PropertyListingsContext.Provider>
+      )
+    }
+  }
