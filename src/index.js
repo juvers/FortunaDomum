6f7f2acd831675a42b57d78fad590e30b6dfc46c
Jude kuti <judekuti@gmail.com>
2020-08-14 23:23:53 -0500

    Experimenting with @reach/router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from '@reach/router'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <App default/>
+    </Router>,
   document.getElementById('root')
 );
 
