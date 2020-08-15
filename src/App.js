d00c46dbfb9344c8e02a873f7c84f47762d60d9f
Jude kuti <judekuti@gmail.com>
2020-08-14 23:42:54 -0500

    Test map with hard coded address

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Map from './components/map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Starter Page
-      </header>
+     <Map />
     </div>
   );
 }
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function replaceAll(target, search, replacement) {
-  return target.replace(new RegExp(search, 'g'), replacement)
-}
+// function replaceAll(target, search, replacement) {
+//   return target.replace(new RegExp(search, 'g'), replacement)
+// }
 
-function Map({ address }) {
-  const replaced = replaceAll(address, ' ', '%20')
-  const search = `https://maps.google.com/maps?q=${replaced}&t=&z=16&ie=UTF8&iwloc=&output=embed`
+function Map() {
+//   const replaced = replaceAll(address, ' ', '%20')
+//   const search = `https://maps.google.com/maps?q=${replaced}&t=&z=16&ie=UTF8&iwloc=&output=embed`
 
+  const address = '555 Bridle Path, Grand Prairie, Texas, 75050'
+  const search = `https://maps.google.com/maps?q=${address}&t=&z=16&ie=UTF8&iwloc=&output=embed`
   return (
     <div className="mapouter">
       <div className="gmap_canvas">
